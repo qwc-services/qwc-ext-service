986deb7169ec53d0f3ebe1853532434ec794c550
Sandro Mani <manisandro@gmail.com>
2026-02-06 18:39:29 +0100

    Allow setting local webserver via FLASK_RUN_PORT

--- src/server.py
+++ src/server.py
@@ -113,4 +113,4 @@ def healthz():
 # local webserver
 if __name__ == '__main__':
     print("Starting external link service...")
-    app.run(host='localhost', port=5023, debug=True)
+    app.run(host='localhost', port=os.environ.get("FLASK_RUN_PORT", 5000), debug=True)
